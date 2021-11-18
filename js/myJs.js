8aea70ebd625896cac5313dfcff37697b95b87f9
b0pb1pr0 <94599214+b0pb1pr0@users.noreply.github.com>
2021-11-18 15:54:04 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,17 +1,17 @@
 const textConfig = {
-  text1: "He luu cậu!",
+  text1: "He luu Trương Bảo Nhi!",
   text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
-  text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
+  text3: "Cậu có bị hâm ko ? ",
+  text4: "Nếu cậu ko trả lời mà thoát ra tức là cậu bị hâm đó nha :)) ",
+  text5: "Đéo???",
+  text6: "Đương nhiên là có rồi :)))",
+  text7: "Cậu thấy tớ thế nào ?",
   text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text9: "Cậu không những đẹp trai lại còn giỏi :)))",
+  text10: "Tớ biết mà , ngại quá hehe ^^",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
-  text12: "Okii lunn <3",
+    "tks nha ^^",
+  text12: "tớ chỉ nói sự thật thui ^^",
 };
 
 $(document).ready(function () {
